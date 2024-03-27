7d6b90a8c2bd13cdd2fe8d5684358f4609c1db97
IDziubek <idziubekk@gmail.com>
2024-03-27 22:59:35 +0100

    Add Book and Loan Controllers auth check

--- src/main/java/lib/edu/libraryapp/controller/BookController.java
+++ src/main/java/lib/edu/libraryapp/controller/BookController.java
@@ -7,6 +7,7 @@ import lib.edu.libraryapp.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,21 +24,25 @@ public class BookController {
     }
 
     @GetMapping()
+    @PreAuthorize("hasAnyRole()")
     public List<GetBookDto> getAllBooks() {
         return bookService.getAll();
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole()")
     public GetBookDto getOne(@PathVariable long id){
         return bookService.getOne(id);
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<CreateBookResponseDto> create(@RequestBody CreateBookDto book) {
         var newBook = bookService.create(book);
         return new ResponseEntity<>(newBook, HttpStatus.CREATED);
     }
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<Void> delete(@PathVariable long id){
         bookService.delete(id);
         return ResponseEntity.noContent().build();
--- src/main/java/lib/edu/libraryapp/controller/LoanController.java
+++ src/main/java/lib/edu/libraryapp/controller/LoanController.java
@@ -5,6 +5,7 @@ import lib.edu.libraryapp.controller.dto.loan.BeginLoanResponseDto;
 import lib.edu.libraryapp.controller.dto.loan.GetLoanDto;
 import lib.edu.libraryapp.service.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,21 +21,25 @@ public class LoanController {
         this.loanService = loanService;
     }
     @GetMapping()
+    @PreAuthorize("hasRole('Admin')")
     public List<GetLoanDto> getAll() {
         return loanService.getAll();
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public GetLoanDto getOne(@PathVariable long id){
         return loanService.getOne(id);
     }
 
     @PostMapping()
+    @PreAuthorize("hasRole('Admin')")
     public BeginLoanResponseDto beginLoan(@RequestBody BeginLoanDto beginLoanDto){
         return loanService.beginLoan(beginLoanDto);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('Admin')")
     public GetLoanDto endLoan(@PathVariable long id){
         return loanService.endLoan(id);
     }
